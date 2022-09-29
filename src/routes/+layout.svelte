8bacbdecfc615b5db88515c53b7ffc2d14f48474
Ak4zh <agwl.akash@gmail.com>
2022-09-29 18:02:57 +0530

    added nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,27 @@
 	import '@brainandbones/skeleton/themes/theme-vintage.css';
 	import '@brainandbones/skeleton/styles/all.css';
 	import '../app.postcss';
+	import { AppBar, AppShell, GradientHeading, LightSwitch } from '@brainandbones/skeleton';
+
 </script>
 
-<slot />
+
+
+<div class="bg-hero-topography">
+	<AppShell>
+		<svelte:fragment slot="pageHeader">
+			<AppBar>
+				<svelte:fragment slot="lead">
+					<GradientHeading tag="h1" class="text-3xl font-bold" direction="bg-gradient-to-r" from="from-primary-500" to="to-accent-500">Hoshana Events</GradientHeading>
+				</svelte:fragment>
+
+				<svelte:fragment slot="trail">
+					<LightSwitch />
+				</svelte:fragment>
+			</AppBar>
+		</svelte:fragment>
+		<!-- Router Slot -->
+		<slot />
+	
+	</AppShell>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,17 @@ const config = {
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
-		require("@brainandbones/skeleton/tailwind/theme.cjs")
+		require("@brainandbones/skeleton/tailwind/theme.cjs"),
+		require("tailwind-heropatterns")({
+			variants: [],
+			patterns: ["polka-dots", "topography"],
+			colors: {
+			  default: "#9C92AC"
+			},
+			opacity: {
+			  default: "0.3"
+			}
+		})
 	]
 };
 
